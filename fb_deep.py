77da447c9002a04b3c56e10d8d3e9bf2fe8c25b5
marony <marony@binbo-kodakusan.com>
2017-11-01 09:41:09 +0900

    change file encoding

--- fb_deep.py
+++ fb_deep.py
@@ -2,28 +2,28 @@ import tensorflow as tf
 import numpy as np
 
 """
-TensorFlow$B$N>pJs$r%0%0$j$^$/$C$F:n$C$?5!3#3X=,(BFizzBuzz
-$B3FAX$NLr3d$H$+?tCM$,E,Ev$+$H$+$h$/$o$+$C$F$J$$(B
+TensorFlowの情報をググりまくって作った機械学習FizzBuzz
+各層の役割とか数値が適当かとかよくわかってない
 """
 
-# $B=i4|CM(B
-# $B@0?t(BMAX$BCM(B
+# 初期値
+# 整数MAX値
 M = 100
-# $BF~NO%Y%/%H%k$NBg$-$5(B(8bit$B@0?t(B)
+# 入力ベクトルの大きさ(8bit整数)
 N = 8
-# $B%/%i%9?t(B(None, Fizz, Buzz, FizzBuzz)
+# クラス数(None, Fizz, Buzz, FizzBuzz)
 C = 4
-# $B71N}2s?t(B
+# 訓練回数
 T = 10000
 L = 0.1
-# $B1#$lAX(B
+# 隠れ層
 H1 = 64
 H2 = 64
 
 def fizz_buzz(n):
     """
-    $B@0?t$K1~$8$F(BFizzBuzz$BJ8;zNs$r=PNO$9$k(B
-    FizzBuzz$B$N@52r:n@.$K;HMQ(B
+    整数に応じてFizzBuzz文字列を出力する
+    FizzBuzzの正解作成に使用
     """
     if n % 15 == 0:
         return "FizzBuzz"
@@ -35,7 +35,7 @@ def fizz_buzz(n):
 
 def fb_to_one_hot(fb):
     """ 
-    FizzBuzz$BJ8;zNs$+$i$=$N%/%i%9$rI=$9%Y%/%H%k$r:n@.(B(one_hot$B$H$$$&!)!)!)(B)
+    FizzBuzz文字列からそのクラスを表すベクトルを作成(one_hotという？？？)
     """
     if fb == "Fizz":
         return [0, 1, 0, 0]
@@ -47,7 +47,7 @@ def fb_to_one_hot(fb):
 
 def one_hot_to_fb(n, a):
     """
-    FizzBuzz$B$N%/%i%9$rI=$9%Y%/%H%k$+$i(BFizzBuzz$BJ8;zNs$KJQ49(B
+    FizzBuzzのクラスを表すベクトルからFizzBuzz文字列に変換
     """
     i = np.argmax(a)
     if i == 1:
@@ -60,7 +60,7 @@ def one_hot_to_fb(n, a):
 
 def n_to_a(n):
     """
-    $B@0?t$r(B2$B?J?t$rI=$9(B8$BMWAG%Y%/%H%k$KJQ49(B
+    整数を2進数を表す8要素ベクトルに変換
     """
     a = np.zeros(N)
     for i in range(N):
@@ -71,7 +71,7 @@ def n_to_a(n):
 
 def create_training_data():
     """
-    $B71N}MQ(B($B@52r(B)$B%G!<%?$N:n@.(B                                                                                         
+    訓練用(正解)データの作成                                                                                         
     """
     x_data = []
     y_data = []
@@ -85,8 +85,8 @@ def create_training_data():
 
 def model(x, weights, biases):
     """
-    $B%b%G%k$N:n@.(B
-    $BF~NO(B(N)$B"*1#$l(B1(N, H1)$B"*1#$l(B2(H1, H2)$B"*=PNO(B(H2, C)
+    モデルの作成
+    入力(N)→隠れ1(N, H1)→隠れ2(H1, H2)→出力(H2, C)
     """
     with tf.name_scope('hidden1'):
         layer_1 = tf.matmul(x, weights['h1']) + biases['b1']
@@ -100,7 +100,7 @@ def model(x, weights, biases):
 
 def loss(logits, labels):
     """
-    $B3X=,$N$J$K$+(B($B$h$/$o$+$C$F$J$$(B)
+    学習のなにか(よくわかってない)
     """
 #    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels)
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits)
@@ -118,14 +118,14 @@ biases = {
     'out': tf.Variable(tf.zeros([C]))
 }
 
-# $B%b%G%k:n@.(B
+# モデル作成
 x = tf.placeholder(tf.float32, [None, N])
 y = tf.placeholder(tf.float32, [None, C])
 
 # Construct model
 pred = model(x, weights, biases)
 
-# $B3X=,$N$J$K$+(B($B$h$/$o$+$C$F$J$$(B
+# 学習のなにか(よくわかってない
 loss = loss(pred, y)
 train_step = tf.train.GradientDescentOptimizer(L).minimize(loss)
 #optimizer = tf.train.AdamOptimizer(learning_rate=L).minimize(cost)
@@ -133,16 +133,16 @@ train_step = tf.train.GradientDescentOptimizer(L).minimize(loss)
 
 init = tf.global_variables_initializer()
 
-# $B%;%C%7%g%s3+;O(B
+# セッション開始
 with tf.Session() as sess:
     sess.run(init)
 
-    # $B71N}(B
+    # 訓練
     x_data, y_data = create_training_data()
     for _ in range(T):
         sess.run(train_step, feed_dict={x: x_data, y: y_data})
 
-    # $B71N}7k2L=PNO(B
+    # 訓練結果出力
     for i in range(30):
         n = i + 1
         xx = [n_to_a(n)]
--- fb_nn.py
+++ fb_nn.py
@@ -2,25 +2,25 @@ import tensorflow as tf
 import numpy as np
 
 """
-TensorFlow$B$N!V(BMNIST For ML Beginners $B!W$NDL$j$N5!3#3X=,(BFizzBuzz
-$B@5>o$K(BFizzBuzz$B$G$-$J$$(B
-$BD>@~$G%/%i%9$o$1$7$F$k$N$,M}M3$@$H;W$&$1$I4V0c$C$F$k$+$b(B
+TensorFlowの「MNIST For ML Beginners 」の通りの機械学習FizzBuzz
+正常にFizzBuzzできない
+直線でクラスわけしてるのが理由だと思うけど間違ってるかも
 """
 
-# $B=i4|CM(B
-# $B@0?t(BMAX$BCM(B
+# 初期値
+# 整数MAX値
 M = 100
-# $BF~NO%Y%/%H%k$NBg$-$5(B(8bit$B@0?t(B)
+# 入力ベクトルの大きさ(8bit整数)
 N = 8
-# $B%/%i%9?t(B(None, Fizz, Buzz, FizzBuzz)
+# クラス数(None, Fizz, Buzz, FizzBuzz)
 C = 4
-# $B71N}2s?t(B
+# 訓練回数
 T = 10000
 
 def fizz_buzz(n):
     """
-    $B@0?t$K1~$8$F(BFizzBuzz$BJ8;zNs$r=PNO$9$k(B
-    FizzBuzz$B$N@52r:n@.$K;HMQ(B
+    整数に応じてFizzBuzz文字列を出力する
+    FizzBuzzの正解作成に使用
     """
     if n % 15 == 0:
         return "FizzBuzz"
@@ -32,7 +32,7 @@ def fizz_buzz(n):
 
 def fb_to_one_hot(fb):
     """
-    FizzBuzz$BJ8;zNs$+$i$=$N%/%i%9$rI=$9%Y%/%H%k$r:n@.(B(one_hot$B$H$$$&!)!)!)(B)
+    FizzBuzz文字列からそのクラスを表すベクトルを作成(one_hotという？？？)
     """
     if fb == "Fizz":
         return [0, 1, 0, 0]
@@ -44,7 +44,7 @@ def fb_to_one_hot(fb):
 
 def one_hot_to_fb(n, a):
     """
-    FizzBuzz$B$N%/%i%9$rI=$9%Y%/%H%k$+$i(BFizzBuzz$BJ8;zNs$KJQ49(B
+    FizzBuzzのクラスを表すベクトルからFizzBuzz文字列に変換
     """
     i = np.argmax(a)
     if i == 1:
@@ -57,7 +57,7 @@ def one_hot_to_fb(n, a):
 
 def n_to_a(n):
     """
-    $B@0?t$r(B2$B?J?t$rI=$9(B8$BMWAG%Y%/%H%k$KJQ49(B
+    整数を2進数を表す8要素ベクトルに変換
     """
     a = np.zeros(N)
     for i in range(N):
@@ -68,7 +68,7 @@ def n_to_a(n):
 
 def create_training_data():
     """
-    $B71N}MQ(B($B@52r(B)$B%G!<%?$N:n@.(B
+    訓練用(正解)データの作成
     """
     x_data = []
     y_data = []
@@ -80,7 +80,7 @@ def create_training_data():
         y_data.append(fb_to_one_hot(fb))
     return (x_data, y_data)
 
-# $B%b%G%k:n@.(B
+# モデル作成
 x = tf.placeholder(tf.float32, [None, N])
 y_ = tf.placeholder(tf.float32, [None, C])
 
@@ -89,22 +89,22 @@ b = tf.Variable(tf.zeros([C]))
 
 y = tf.matmul(x, W) + b
 
-# $B3X=,$N$J$K$+(B($B$h$/$o$+$C$F$J$$(B)
+# 学習のなにか(よくわかってない)
 #cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)), reduction_indices=[1]))
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-# $B%;%C%7%g%s3+;O(B
+# セッション開始
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
 
-    # $B71N}(B
+    # 訓練
     x_data, y_data = create_training_data()
     for _ in range(T):
         train_step.run(feed_dict={x: x_data, y_: y_data})
 
-    # $B71N}7k2L=PNO(B
+    # 訓練結果出力
     for i in range(30):
         n = i + 1
         xx = [n_to_a(n)]
